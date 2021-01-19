cd21f5f5cdc15f50146245957b7a18d6dd5aaffd
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-01-18 22:37:10 -0300

    First push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.thrashermagazine.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Una pagina de skate real
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
