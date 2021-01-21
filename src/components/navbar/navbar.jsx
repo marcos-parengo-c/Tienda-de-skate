99169a0bebcf0b71ffd781a4f44e5833345be808
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-01-21 01:09:17 -0300

    navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+let Nav = () =>{
+    return(
+        <nav>
+            <div className="logo"><h1>Marki</h1></div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Some</li>
+                <li>Some</li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav
