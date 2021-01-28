a0a3a3c3b12fe4562b5a1e91f7179c6f0b2333ea
marki <marcos.sk8.parengo@gmail.com>
2021-01-27 21:49:38 -0300

    Correccion de 3er desafio

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import background from './background.webp'
-import './index.css'
+import React, {Component, useState} from 'react';
+import background from './background.webp';
+import './index.css';
 
 const ItemListContainer = ({children, greeting}) => {
     return (
         <div>
             <img src={background} width="100%"/>
-            <h1 className="centered">{greeting}</h1>
+            <h1>{greeting}</h1>
         </div>
     )
 }
