c69d4e0e2f40fd096981e4925e677ec0e721cc7d
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-02-01 20:40:06 -0300

    Titulo y stock en la card del producto

--- src/containers/ItemListContainer/Contador/index.jsx
+++ src/containers/ItemListContainer/Contador/index.jsx
@@ -5,31 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Contador = ({ firstStock }) => {
     const [contador, setContador] = useState(0)
     const [stock, setStock] = useState(firstStock)
-    
+
     return (
-                <Col>
-                    <div className="card">
-                        <div class="card-body">
-                            <div class="d-grid gap-2 col-6 ml-auto">
-                                <div class="d-flex justify-content-start">
-                                    <button type="button" className="btn btn-primary" onClick={() => { if (contador > 0) { setContador(contador - 1) } }}>-</button>
-                                    <input type="number" className="num" value={contador} />
-                                    <button type="button" className="btn btn-primary" onClick={() => {
-                                         if(contador<stock){
-                                             setContador(contador + 1)
-                                            }else{alert('No podes añadir mas que estos elementos ya que el stock es de '+stock)}
-                                             }}>+</button>
-                                </div>
-                                <button type="button" className="btn btn-primary"
-                                    onClick={() => {
-                                            setContador(0);
-                                            setStock(stock-contador)
-                                    }}>Add to cart
-                            </button>
-                            </div>
+        <Col>
+            <div className="card">
+                <div class="card-body">
+                    <h5 className="card-title">Producto</h5>
+                    <p className="card-text">{stock} productos diponibles.</p>
+                    <div class="d-grid gap-2 col-12 ml-auto">
+                        <div class="d-flex justify-content-start">
+                            <button type="button" className="btn btn-primary" onClick={() => { if (contador > 0) { setContador(contador - 1) } }}>-</button>
+                            <input type="number" className="num" value={contador} />
+                            <button type="button" className="btn btn-primary" onClick={() => {
+                                if (contador < stock) {
+                                    setContador(contador + 1)
+                                } else { alert('No podes añadir mas que estos elementos ya que el stock es de ' + stock) }
+                            }}>+</button>
                         </div>
+                        <button type="button" className="btn btn-primary"
+                            onClick={() => {
+                                setContador(0);
+                                setStock(stock - contador)
+                            }}>Add to cart
+                            </button>
                     </div>
-                </Col>
+                </div>
+            </div>
+        </Col>
     )
 }
 
