bc457699a6bb85e23be006cbf878bb524ac5f3e6
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-02-03 21:37:33 -0300

    Desafio 6 con promise

--- src/containers/ItemListContainer/ItemList/index.jsx
+++ src/containers/ItemListContainer/ItemList/index.jsx
@@ -3,12 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Item from './Item'
-import CardDeck from 'react-bootstrap/CardDeck';
 
 const ItemList = ({ itemProp }) => {
 
-    const [item] = useState(itemProp)
-    console.log(item)
+    const [item,setItem] = useState([])
+
+	const promesa = new Promise((result, reject) => {
+		setTimeout(function(){ result(itemProp)}, 3000);		
+	})
+    
+	promesa.then(result => {
+        console.log(result) 
+        setItem(result)
+	}, err => {
+		console.log(err) 
+	}).catch(err => {console.log("atrapado")
+					}).finally(()=>{
+		console.log("finalizado")
+    })
+    
+    
     return (
         <>
             <Col sm={8}>
--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -9,9 +9,8 @@ import ItemList from './ItemList'
 
 const ItemListContainer = ({ children, greeting }) => {
 
-    const [producto, setProducto] = useState(0)
     const [items] = useState([{name:"Zapatilla",price:2000,image:"images/Zapatilla.jpg"},{name:"Buzo",price:3000,image:"images/Buzo.jpg"},{name:"Pantalon",price:4000,image:"images/Pantalon.jpg"}])
-
+    
     return (
         <div className="container">
             <Row>
