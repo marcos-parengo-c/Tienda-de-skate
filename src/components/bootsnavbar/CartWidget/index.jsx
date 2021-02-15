46da94f28a8fff838fcd13d10b0241462281c347
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-02-14 22:28:19 -0300

    NavBar lista (carrito a la derecha pero adentro del collapse)

--- src/components/bootsnavbar/CartWidget/index.jsx
+++ src/components/bootsnavbar/CartWidget/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-import cart from "./cart.png"
+
 
 const CartWidget = () => {
     return (
         <Nav.Link as={Link} to={"/cart"}>
             <img
-                alt=""
-                src={cart}
+                alt="verga"
+                src="/images/cart.png"
                 width="30"
                 height="30"
                 className="d-inline-block align-top" />
