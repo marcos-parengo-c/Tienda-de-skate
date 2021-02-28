4b755c5982bd94a7edaeb00226e3cb79c49929f6
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-02-28 16:11:25 -0300

    Item list cargado de firebase

--- src/containers/ItemListContainer/index.jsx
+++ src/containers/ItemListContainer/index.jsx
@@ -1,42 +1,40 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Row from 'react-bootstrap/Row';
 import ItemList from './ItemList'
-import { productContext } from '../../context/productContext';
-import { getFirestore } from '../../firebase';
+// import { getFirestore } from '../../firebase';
 
 const ItemListContainer = ({ children, greeting, listaDeItems }) => {
 
     const { id } = useParams();
-    const itemCntxt = useContext(productContext)
-    const [items, setItems] = useState(itemCntxt)
+    const [categoryId, setcategoryId] = useState()
 
     useEffect(() => {
         if (typeof id === 'undefined') {
-            setItems(itemCntxt)
+            setcategoryId(true)
         } else {
-            setItems(itemCntxt.filter(items => items.brand === id));
+            setcategoryId(id);
         }
         //Parte fireBase
-        const baseDeDatos = getFirestore(); 
-        // Guardamos la referencia de la coleccion que queremos tomar
-        const itemCollection = baseDeDatos.collection('productos'); 
-        // Tomando los datos
-        itemCollection.get().then((value) => {
-            value.docs.forEach(element => { console.log({...element.data(), id:element.id})})
-        })
+        // const baseDeDatos = getFirestore(); 
+        // // Guardamos la referencia de la coleccion que queremos tomar
+        // const itemCollection = baseDeDatos.collection('productos'); 
+        // // Tomando los datos
+        // itemCollection.get().then((value) => {
+        //     value.docs.forEach(element => { console.log({...element.data(), id:element.id})})
+        // })
         //Fin Parte Firebase
 
         return () => {
         }
-    }, [id, itemCntxt])
+    }, [id])
 
     return (
         <div className="container">
             <Row>
-                <ItemList itemProp={items} />
+                <ItemList id={categoryId} />
             </Row>
         </div>
     )
